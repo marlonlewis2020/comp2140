7d900ab29a443823f8de87e361206406c3406fb6
Marlon Lewis <marlonlewis483@gmail.com>
2021-11-09 03:50:31 -0500

    added interface, authentication files in their relevant packages

---
+++ 0-BeaditupjaApp/App/BeadItUpJa/src/ClassInterface/Operations.java
@@ -0,0 +1,30 @@
+package ClassInterface;
+
+public interface Operations {
+
+    /*
+     * Adds the created object to the database
+     */
+    public abstract boolean create();
+
+    /*
+     * Edits the object in the database
+     */
+    public abstract boolean edit();
+
+    /*
+     * Provides a list of objects of this class type from the database
+     */
+    public abstract String[] view();
+
+    /*
+     * Returns the id for this object
+     */
+    public abstract int getID();
+
+    /*
+     * Returns the string identifier for this object
+     */
+    public abstract String toString();
+
+}
