b5f5061419e44d6a1b5bd94f9a3678b3fee2bd2b
Marlon Lewis <marlonlewis483@gmail.com>
2021-11-29 04:42:48 -0500

    fixed tests for stock and authentication. cleaned code

--- src/test.java
+++ src/test.java
@@ -1,3 +1,4 @@
+
 import Authentication.Authentication;
 class test {
     
@@ -6,76 +7,87 @@ class test {
         /**
          * Authentication Tests
          */
-        Authentication auth = new Authentication();
-        Authentication none = new Authentication();
-        System.out.println("role: "+auth.authenticate("mlewis","1403730359"));
-        System.out.println("role: "+none.authenticate("user", "pw"));
-        
-        //testing login user's menu options
-        for(String s : auth.getUserMenu()){
-            System.out.println("Authenticated User's Menu item: "+s);
-        }
-        //testing unauthenticated user's menu options
-        for(String s : none.getUserMenu()){
-            System.out.println("Logged Out User's Menu item: "+s);
-        }
+        try {
+            Authentication auth = new Authentication();
+            Authentication none = new Authentication(); 
+            System.out.println("role: "+auth.authenticate("mlewis","1403730359"));
+            System.out.println("role: "+none.authenticate("user", "pw"));
+            
+            //testing login user's menu options
+            for(String s : auth.getUserMenu()){
+                System.out.println("Authenticated User's Menu item: "+s);
+            }
 
-        //confirming login/logout changes to state
-        System.out.println("'auth' authenticated: "+auth.getAuth_message());
-        System.out.println("'none' failed authentication: "+none.getAuth_message());
-        auth.authenticate("", "");
-        System.out.println("auth signed out: "+auth.getAuth_message());
-        none.authenticate("mlewis","1403730359");
-        System.out.println("'none' authenticated: "+none.getAuth_message());
-
+            //confirming login/logout changes to state
+            System.out.println("authenticated: "+auth.getAuth_message());
+            System.out.println("NOT authenticated: "+none.getAuth_message()); 
+            auth.authenticate("", "");
+            System.out.println("auth signed out: "+auth.getAuth_message());
+            none.authenticate("mlewis","1403730359");
+            System.out.println("'none' authenticated: "+none.getAuth_message());
+            
+        } catch (Exception e) {
+            // System.out.println(e.getMessage()); 
+            System.out.println("!!!!!AUTHENTICATION EXCEPTION ENCOUNTERED!!!!!");
+        }
         
 
         /**
         * Stock class test
         */
-        Stock gibbs = new Stock (StockType.Beads,"Gibbits", 150,50);
-        gibbs.createStock();
-        int quantity = Stock.getQuantity("Gibbits");
-        System.out.println("Quantity = "+ quantity);
-        Stock.updateStock('-',12, "Gibbits");
-        // Stock.updateStock('-',500, "Gibbits");
-        quantity = Stock.getQuantity("Gibbits");
-        System.out.println("Quantity = "+ quantity);
-        for(Stock s:Stock.viewStock()){
-            System.out.println(s.toString());
+        try {
+            Stock gibbs = new Stock (StockType.Beads,"Gibbits", 150,50);
+            gibbs.createStock();
+            
+            System.out.println("Gibbits Quantity = "+ Stock.getQuantity("Gibbits"));
+            System.out.println("Panther Quantity = "+ Stock.getQuantity("Panther")); 
+            Stock.updateStock('+',499, "Gibbits"); 
+            Stock.updateStock('+',499, "Panther"); 
+            Stock.updateStock('-',500, "Gibbits");
+            Stock.updateStock('-',500, "Panther");
+            System.out.println("Gibbits Quantity = "+ Stock.getQuantity("Gibbits")); 
+            System.out.println("Panther Quantity = "+ Stock.getQuantity("Panther")); 
+            for(Stock s:Stock.viewStock()){
+                System.out.println(s.toString());
+            }
+    
+            Stock sec = new Stock(StockType.Beads,"Panther", 9150,50);
+            ;
+            System.out.println("[TOSTRING() FROM CLASS TEST]: "+sec.toString());
+            sec.createStock();
+            System.out.println("[VIEWITEM(STOCKNAME) TEST]" + Stock.viewItem(sec.getStockName()).toString());
+        } catch (Exception e) {
+            // System.out.println(e.getMessage()); 
+            System.out.println("!!!!!STOCK EXCEPTION ENCOUNTERED!!!!!");
         }
-
-        Stock sec = new Stock(StockType.Beads,"Panther", 9150,50);
-        ;
-        System.out.println(sec.toString());
-        sec.createStock();
-        //Stock.viewItem(sec.getStockName()).toString();
-
-
-
-
-
 
 
         /**
         * Bracelet Tests
         */
 
-        Bracelet.populate();
-        Bracelet b = new Bracelet("b1", 3.00, "yellow-2;green-4", "yellow-5;green-8", "yellow-10;green-16", "diatta");
-        Bracelet.addToArray(b);
-        b.addToDatabase();
-        //System.out.println(b.getName());
-        //System.out.println(b.getBracelets());
-        //System.out.println(b.getID());
-        //System.out.println(b.getCollection());
-        //System.out.println(Bracelet.getBraceletIndex("b1"));
-        // Bracelet b = Bracelet.searchByName("b1");
-        System.out.println(b.getName());
-        System.out.println(b.getSmallBeadQty());
-        System.out.println(b.getLgBeadQty());
-        System.out.println(b.getMedBeadQty());
-        //Bracelet.deleteBracelet("b1");
+        try{
+            Bracelet.populate();
+            Bracelet b; 
+            b = new Bracelet("b1", 3.00, "yellow-2;green-4", "yellow-5;green-8", "yellow-10;green-16", "diatta");
+            //System.out.println(b.getName());
+            //System.out.println(b.getBracelets());
+            //System.out.println(b.getID());
+            //System.out.println(b.getCollection());
+            //System.out.println(Bracelet.getBraceletIndex("b1"));
+            // Bracelet b = Bracelet.searchByName("b1");
+            Bracelet.addToArray(b);
+            b.addToDatabase();
+            System.out.println(b.getName());
+            System.out.println(b.getSmallBeadQty());
+            System.out.println(b.getLgBeadQty());
+            System.out.println(b.getMedBeadQty());
+            //Bracelet.deleteBracelet("b1");
+        }
+        catch(Exception e){
+            // System.out.println(e.getMessage()); 
+            System.out.println("!!!!!BRACELET EXCEPTION ENCOUNTERED!!!!!");
+        }
 
 }
 }
