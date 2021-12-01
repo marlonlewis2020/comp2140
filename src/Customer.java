e2faa6a8343ca1f891f82c9c37956582f42590a9
Mercedes Smith <mercedes.smith@mymona.uwi.edu>
2021-12-01 11:23:46 -0500

    Removed line 4

--- src/Customer.java
+++ src/Customer.java
@@ -1,8 +1,5 @@
 import java.sql.Connection;
 import java.util.ArrayList;
-
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import Authentication.Authentication;
