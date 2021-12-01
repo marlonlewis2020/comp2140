34233c8780fe94f93e8558dc1718e5edb6a9500c
Mercedes Smith <mercedes.smith@mymona.uwi.edu>
2021-12-01 13:58:32 -0500

    updateCustomer completed

--- src/test.java
+++ src/test.java
@@ -28,14 +28,18 @@ class test {
         //Deleting customer
         Customer.deleteCustomer(Customer.getCusId("Taye-Vaughn", "8769654781", "Mobay"));
 
+        //Testing update customer()
+        Customer c = Customer.search("Gabriel","8769654781");
+        c.updateCustomer("Gabriel", "8769654781", "", "", "Portland");
 
         //Testing populate()
         for (int i = 0; i < Customer.getCustomers().size(); i++)
         {
             System.out.println(Customer.getCustomers().get(i).getcustomerName());
+            System.out.println(Customer.getCustomers().get(i).getpickupLocation());
         } 
 
-        //Testing update customer()
+        
     
 
 
